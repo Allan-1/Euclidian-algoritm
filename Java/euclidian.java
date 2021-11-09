752e7e852c619deedbd0b8393de749f47e90d1c3
Allan Muturi <amuturi01@gmail.com>
2021-11-09 17:26:57 +0300

    solving java logic

--- Java/euclidian.java
+++ Java/euclidian.java
@@ -2,30 +2,25 @@ package Java;
 
 class Euclidian{
     static int gcd(int x, int y){
-        if(y > x){
-            int temp = y;
-            y = x;
-            x = temp;
-        }else{
-        }
         while (true){
-            if(x % y == 0){
-                return x / y;
-            }else{
+            while(true){
+                if(x % y == 0){
+                    return y;
+                }
                 int a = x % y;
                 int b = y % a;
                 if (a == 0){
                     return b;
                 }else if(b == 0){
                     return a;
-                }else{
-                    continue;
                 }
+                a = a % b;
+                b = b % a;
             }
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(gcd(12, 6));
+        System.out.println(gcd(15, 6));
     }
 }
